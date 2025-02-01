ec083643f2b16e864131dd3e426da2473f778340
Darlley <systembrtt@gmail.com>
2025-02-01 19:11:30 -0400

    fix: desacoplamento de classes

--- login.js
+++ login.js
@@ -1,25 +1,35 @@
+// signup-router
 const express = require('express')
 const router = express.Router()
-const mongoose = require('mongoose')
-const AccountModel = mongoose.model('Account')
 
 module.exports = () => {
   router.post('/signup', new SignUpRouter().route) // .route é um ponteiro que ja recebe no callback os parametros
 }
 
-/**
- * Classe sobrecarregada:
- * - validação do req, res
- * - regra de negócio (password e repeartPassword precisam ser iguais)
- * - acesso ao banco de dados
- */
 class SignUpRouter {
   async route (req, res) {
     const { email, password, repeatPassword } = req.body
-    if (password === repeatPassword) {
-      const user = await AccountModel.create({ email, password })
-      return res.json(user)
-    }
+    new SignUpUseCase().signUp(email, password, repeatPassword)
     res.status(400).json({ error: 'password must be equal to repeatPassword' })
+  }
+}
+
+// signup-usecase
+class SignUpUseCase {
+  async signUp (email, password, repeatPassword) {
+    if (password === repeatPassword) {
+      new AddAccountRepository().add(email, password)
+    }
+  }
+}
+
+// add-account-repo
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+class AddAccountRepository {
+  async add (email, password) {
+    const user = await AccountModel.create({ email, password })
+    return user
   }
 }
