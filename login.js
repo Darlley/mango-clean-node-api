d02a956bd069b16dd7933118ed2d45c65299e986
Darlley <systembrtt@gmail.com>
2025-02-01 18:51:23 -0400

    feat: login route express

---
+++ login.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+const mongoose = require('mongoose')
+const AccountModel = mongoose.model('Account')
+
+/**
+ * Este código esta ilegível
+ */
+module.exports = () => {
+  router.post('/signup', async (req, res) => {
+    const { email, password, repeatPassword } = req.body
+    if (password === repeatPassword) {
+      const user = await AccountModel.create({ email, password })
+      return res.json(user)
+    }
+    res.status(400).json({ error: 'password must be equal to repeatPassword' })
+  })
+}
