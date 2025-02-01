cfff2db3f8e9d26b91cb135f6c3113ca94b3305c
Darlley <systembrtt@gmail.com>
2025-02-01 18:58:02 -0400

    fix: class signup

--- login.js
+++ login.js
@@ -3,16 +3,23 @@ const router = express.Router()
 const mongoose = require('mongoose')
 const AccountModel = mongoose.model('Account')
 
-/**
- * Este código esta ilegível
- */
 module.exports = () => {
-  router.post('/signup', async (req, res) => {
+  router.post('/signup', new SignUpRouter().route) // .route é um ponteiro que ja recebe no callback os parametros
+}
+
+/**
+ * Classe sobrecarregada:
+ * - validação do req, res
+ * - regra de negócio (password e repeartPassword precisam ser iguais)
+ * - acesso ao banco de dados
+ */
+class SignUpRouter {
+  async route (req, res) {
     const { email, password, repeatPassword } = req.body
     if (password === repeatPassword) {
       const user = await AccountModel.create({ email, password })
       return res.json(user)
     }
     res.status(400).json({ error: 'password must be equal to repeatPassword' })
-  })
+  }
 }
