fccb9901bfbfcb96277151995a72ed57705c5136
Darlley <systembrtt@gmail.com>
2025-02-02 11:33:42 -0400

    feat: ensure LoginRouter returns 400 if no email is provider

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,5 +1,26 @@
-describe('first', () => {
-  test('should first', () => {
-    console.log('')
+class LoginRouter {
+  route (httpRequest) {
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400
+      }
+    }
+  }
+}
+
+describe('Login router', () => {
+  test('should return 400 if no email is provider', () => {
+    // Arrange (Preparar)
+    const sut = new LoginRouter() // system under test
+    const httpRequest = {
+      body: {
+        // email: 'example@mail.com' # primeiro queremos testar se o email não existe
+        password: 'any_password'
+      }
+    }
+    // Action (Executar)
+    const httpResponse = sut.route(httpRequest)
+    // Assert (Validar)
+    expect(httpResponse.statusCode).toBe(400)
   })
 })
