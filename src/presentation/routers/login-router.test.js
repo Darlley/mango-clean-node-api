7fa2b73c96c51213338924cfc5e121d5c062a9cb
Darlley <systembrtt@gmail.com>
2025-02-02 11:40:46 -0400

    feat: ensure LoginRouter returns 400 if no password provider

--- src/presentation/routers/login-router.test.js
+++ src/presentation/routers/login-router.test.js
@@ -1,6 +1,6 @@
 class LoginRouter {
   route (httpRequest) {
-    if (!httpRequest.body.email) {
+    if (!httpRequest.body.email || !httpRequest.body.password) {
       return {
         statusCode: 400
       }
@@ -9,13 +9,31 @@ class LoginRouter {
 }
 
 describe('Login router', () => {
+
+  // Se não existe email = 400
   test('should return 400 if no email is provider', () => {
     // Arrange (Preparar)
     const sut = new LoginRouter() // system under test
     const httpRequest = {
       body: {
-        // email: 'example@mail.com' # primeiro queremos testar se o email não existe
+        // email: 'example@mail.com'
         password: 'any_password'
+      }
+    }
+    // Action (Executar)
+    const httpResponse = sut.route(httpRequest)
+    // Assert (Validar)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+
+  // Se não existe password = 400
+  test('should return 400 if no password is provider', () => {
+    // Arrange (Preparar)
+    const sut = new LoginRouter() // system under test
+    const httpRequest = {
+      body: {
+        email: 'example@mail.com'
+        // password: 'any_password'
       }
     }
     // Action (Executar)
