93b06127d06ab32437c297cc5d2034d3129a3774
Darlley <systembrtt@gmail.com>
2025-02-11 17:59:20 -0400

    feat: ensure AuthUseCaseSpy calls LoadUserByEmailRepositorySpy with correct email

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,6 +1,10 @@
 const { MissingParamError } = require('../../utils/errors')
 
 class AuthUseCaseSpy {
+  constructor (loadUserByEmailRepository) {
+    this.loadUserByEmailRepository = loadUserByEmailRepository
+  }
+
   async auth (email, password) {
     if (!email) {
       throw new MissingParamError('email')
@@ -8,6 +12,8 @@ class AuthUseCaseSpy {
     if (!password) {
       throw new MissingParamError('password')
     }
+
+    await this.loadUserByEmailRepository.load(email)
   }
 }
 
@@ -24,5 +30,18 @@ describe('Auth UseCase', () => {
     const sut = new AuthUseCaseSpy()
     const promise = sut.auth('any_example@mail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  it('should call LoadUserByEmailRepository with correct email', async () => {
+    class LoadUserByEmailRepositorySpy {
+      async load (email) {
+        this.email = email
+      }
+    }
+    const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+
+    const sut = new AuthUseCaseSpy(loadUserByEmailRepositorySpy)
+    await sut.auth('any_example@mail.com', 'valid_password')
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_example@mail.com')
   })
 })
