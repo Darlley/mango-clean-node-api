db4a72eed9bbc97d862dd989b38956d3786e77dc
Darlley <systembrtt@gmail.com>
2025-02-20 14:47:49 -0400

    enable CORS

--- src/main/config/setup.js
+++ src/main/config/setup.js
@@ -1,3 +1,10 @@
 module.exports = (app) => {
   app.disable('x-powered-by')
+  app.use((req, res, next) => {
+    // midleware para liberar CORS
+    res.set('access-control-allow-origin', '*')
+    res.set('access-control-allow-methods', '*')
+    res.set('access-control-allow-headers', '*')
+    next()
+  })
 }
--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -4,8 +4,16 @@ const app = require('./app')
 
 describe('Setup App', () => {
   it('should disable x-powered-by header', async () => {
-    app.get('/test-x-powered-by', (_, res) => res.send(''))
-    const response = await request(app).get('/test-x-powered-by')
+    app.get('/test_x_powered_by', (_, res) => res.send(''))
+    const response = await request(app).get('/test_x_powered_by')
     expect(response.headers['x-powered-by']).toBeUndefined()
+  })
+
+  it('should enable CORS', async () => {
+    app.get('/test_cors', (_, res) => res.send(''))
+    const response = await request(app).get('/test_cors')
+    expect(response.headers['access-control-allow-origin']).toBe('*')
+    expect(response.headers['access-control-allow-methods']).toBe('*')
+    expect(response.headers['access-control-allow-headers']).toBe('*')
   })
 })
