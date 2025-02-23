eec027d476bccc0115fff44c0015ad681325097c
Darlley <systembrtt@gmail.com>
2025-02-22 22:03:22 -0400

    move loginRouter composition to its own file

---
+++ src/main/composers/login-router-composer.js
@@ -0,0 +1,26 @@
+const LoginRouter = require('../../presentation/routers/LoginRouter')
+const AuthUseCase = require('../../domain/usecases/auth-usecase')
+const EmailValidator = require('../../utils/helpers/EmailValidator')
+const LoadUserByEmailRepository = require('../../infra/repositories/load-user-by-email-repository')
+const UpdateAccessTokenRepository = require('../../infra/repositories/update-access-token-repository')
+const Encrypter = require('../../utils/helpers/Encrypter')
+const TokenGenerator = require('../../utils/helpers/TokenGenerator')
+const env = require('../config/env')
+
+const tokenGenerator = new TokenGenerator(env.TOKEN_SECRET)
+const encrypter = new Encrypter()
+const loadUserByEmailRepository = new LoadUserByEmailRepository()
+const updateAccessTokenRepository = new UpdateAccessTokenRepository()
+const emailValidator = new EmailValidator()
+const authUseCase = new AuthUseCase({
+  loadUserByEmailRepository,
+  updateAccessTokenRepository,
+  encrypter,
+  tokenGenerator
+})
+const loginRouter = new LoginRouter({
+  authUseCase,
+  emailValidator
+})
+
+module.exports = loginRouter
--- src/main/config/env.js
+++ src/main/config/env.js
@@ -1,3 +1,4 @@
 module.exports = {
-  MONGO_URL: global.__MONGO_URI__ || 'mongodb://localhost:27017/mongo-api'
+  MONGO_URL: global.__MONGO_URI__ || 'mongodb://localhost:27017/mongo-api',
+  TOKEN_SECRET: process.env.TOKEN_SECRET || 'secret'
 }
